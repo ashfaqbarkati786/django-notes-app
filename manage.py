6fabda8a772e6496c1825040e2d03c27df03aba6
ashfaqbarkati786 <107784646+ashfaqbarkati786@users.noreply.github.com>
2025-09-29 15:12:43 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-#writtent to trigger webhook
+#writtent to trigger webhook 12
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notesapp.settings')
